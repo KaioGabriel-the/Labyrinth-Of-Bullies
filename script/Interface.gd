7896782d5cbc0f08ba76d8d2212dc3ace6b88f3b
ArthurV10 <arthurvms10@gmail.com>
2024-06-05 11:14:48 -0300

    add: Update labels menssage sucess

--- script/Interface.gd
+++ script/Interface.gd
@@ -1,7 +1,18 @@
 extends CanvasLayer
-
-onready var dialogBox: DialogBox = get_node("DialogBox")
+onready var dialog_box = $DialogBox
 export (Resource) var newMessage
 
-func add_message(_msg : Array) -> void:
-	dialogBox.add_message(newMessage.msg_queue)
+func _ready():
+	add_text(newMessage)
+	
+func add_text(_msg) -> void:
+	dialog_box.add_message(_msg.msg_queue)
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_home"):
+		var _dialog = [
+			"sexo é bom",
+			"ops, engasgei",
+			"rsrsrsrs"
+		];
+		dialog_box.add_message(_dialog);
