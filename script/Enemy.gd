19ffa5ea6dc8f5e08ad85b1e775c4bbc56ccbab4
Patrocinio <patrocinioluisf@gmail.com>
2024-06-10 14:39:50 -0300

    Added chasing enemy

--- script/Enemy.gd
+++ script/Enemy.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 
+onready var timer = $Timer
 var speed = 50
 var velocity : Vector2 = Vector2.ZERO
 var path : Array = []
 var navigation_path : Navigation2D = null
 var player = null
 
-onready var rangeToPursuit: float = 75.0;
+onready var destinyPos: Vector2 = global_position;
+var patrolling : bool = false;
+onready var rangeToPursuit: float = 150.0;
 
 
 func _ready():
@@ -19,7 +22,9 @@ func _process(delta):
 		navigate()
 		velocity = move_and_slide(velocity)
 #		print("Current Position:", global_position)
+	enemyPatrolling()
 
+## Atualiza o trajeto do inimigo
 func update_references():
 	if Global.navigation != null:
 		navigation_path = Global.navigation
@@ -37,15 +42,14 @@ func generate_path():
 		# Verifica se o player está dentro do range:
 		var _playerPos: Vector2 = player.global_position;
 		var _diff: Vector2 = _playerPos - global_position;
-		
-		print("Distância até o player: ", _diff.length())
+#		print("Distância até o player: ", _diff.length())
 		
 		# Por padrão, o inimigo vai se mover até essa posição.
 		# TODO: Colocar um valor mais apropriado.
-		var _destinyPos = global_position;
-			
+		var _destinyPos : Vector2
 		if _diff.length() < rangeToPursuit:
 			_destinyPos = player.global_position;
+			
 			
 		
 		# Verificar se a posição global e a posição global do player são diferentes
@@ -63,3 +67,12 @@ func navigate():
 	elif path.size() == 1:
 		velocity = global_position.direction_to(player.global_position) * speed
 
+
+func _on_Timer_timeout():
+	var random_direction_x = rand_range(-1,1)
+	var random_direction_y = rand_range(-1,1)
+	var _destinyPos = global_position + Vector2(random_direction_x, random_direction_y)
+
+func enemyPatrolling():
+	if patrolling == true:
+		_on_Timer_timeout()
