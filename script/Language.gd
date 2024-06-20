c89e7eca8b63213bbc12fc9906afcb78ff9c2202
ArthurV10 <arthurvms10@gmail.com>
2024-06-19 22:12:57 -0300

    feat: fixed pause menu transitions, language menu improvements

--- script/Language.gd
+++ script/Language.gd
@@ -3,10 +3,11 @@ extends CanvasLayer
 export var selected: int = 0;
 
 func _ready():
-	pass
+	# Conceder foco ao botão correspondente a linguagem selecionada
+	selected = 0 if Global.languagePtBr else 1;
 
 func _process(delta):
-	var _buttons = $Buttons.get_children()
+	var _buttons = $Buttons.get_children();
 	
 	if Input.is_action_just_pressed("ui_up"):
 		selected = 0;
@@ -14,33 +15,33 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_down"):
 		selected = len(_buttons) - 1;
 	
-	var _dir = int(Input.is_action_just_pressed("ui_right")) - int(Input.is_action_just_pressed("ui_left"))
+	var _dir = int(Input.is_action_just_pressed("ui_right")) - int(Input.is_action_just_pressed("ui_left"));
 	print("dir: ", _dir);
-	selected += _dir
-	selected = clamp(selected, 0, len(_buttons) - 1)
+	selected += _dir;
+	selected = clamp(selected, 0, len(_buttons) - 1);
 	
-	var _button = _buttons[selected] as Button
-	print(_button)
+	var _button = _buttons[selected] as Button;
+	print(_button);
 	_button.call_deferred("grab_focus");
 	changeLanguage();
 
 func _on_LanguageBR_pressed():
-	Global.languagePtBr = true
+	Global.languagePtBr = true;
 
 
 func _on_LanguageEUA_pressed():
-	Global.languagePtBr = false
+	Global.languagePtBr = false;
 
 
 func _on_Back_pressed():
-	Global.transitionToScene("res://scenes/title screen.tscn")
+	Global.transitionToScene("res://scenes/title screen.tscn");
 	
 func changeLanguage():
-	if Global.languagePtBr == true:
-		$Buttons/Back.text = "Voltar"
-		$Label.text = "Selecione a Linguagem"
-		$Label/Label.text = "Selecione a Linguagem"
+	if Global.languagePtBr:
+		$Buttons/Back.text = "Voltar";
+		$Label.text = "Selecione a Linguagem";
+		$Label/Label.text = "Selecione a Linguagem";
 	else:
-		$Buttons/Back.text = "Back"
-		$Label.text = "Select Language"
-		$Label/Label.text = "Select Language"
+		$Buttons/Back.text = "Back";
+		$Label.text = "Select Language";
+		$Label/Label.text = "Select Language";
